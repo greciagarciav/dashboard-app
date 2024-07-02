8cb5c6c49612919f6bd9235a7bcecf36df3e21a2
greciagarciav <garciav.grecia@gmail.com>
2024-07-02 10:30:58 -0500

    Add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,12 @@ import { Suspense } from 'react';
 import { RevenueChartSkeleton, 
         LatestInvoicesSkeleton, 
         CardsSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Overview',
+};
+
 export default async function Page() {   
 
     return (
