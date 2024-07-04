1efce5ac0eb416ee0bf084e6bc5c953ae9eb2ea5
greciagarciav <garciav.grecia@gmail.com>
2024-07-03 20:57:13 -0500

    Add pagination and search. Start buttons

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,41 @@
 import { Metadata } from 'next';
-import { fetchCustomers } from '@/app/lib/data';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchCustomersPages } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import { CreateCustomer } from '@/app/ui/customers/buttons';
+import Search from '@/app/ui/search';
 import Table from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
 
-  const customers = await fetchCustomers();
-  return <Table customers={customers} />;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+        {/* <CreateCustomer /> */}
+      </div>
+      <Table query={query} currentPage={currentPage} />
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} currentPage={currentPage} />
+      </div>
+    </div>
+  )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -195,7 +195,11 @@ export async function fetchCustomers() {
   }
 }
 
-export async function fetchFilteredCustomers(query: string) {
+export async function fetchFilteredCustomers(
+  query: string, 
+  currentPage: number
+) {
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
   noStore();
   try {
     const data = await sql<CustomersTableType>`
@@ -214,6 +218,7 @@ export async function fetchFilteredCustomers(query: string) {
         customers.email ILIKE ${`%${query}%`}
 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
 		ORDER BY customers.name ASC
+    LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `;
 
     const customers = data.rows.map((customer) => ({
@@ -226,6 +231,24 @@ export async function fetchFilteredCustomers(query: string) {
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
+  }
+}
+
+export async function fetchCustomersPages(query: string) {
+  noStore();
+  try {
+    const count = await sql`SELECT COUNT(*)
+    FROM customers
+    WHERE
+      customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`}
+  `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of customers.');
   }
 }
 
